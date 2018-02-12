f4c706aed67b711a53746c4835ca3f3b37223bf3
Kaila Goff and Masen Matthews <kaila@>
2018-02-12 11:19:12 -0800

    set up file structure

---
+++ app.rb
@@ -0,0 +1,13 @@
+require('sinatra')
+require('sinatra/reloader')
+require('pry')
+require('./lib/parcels')
+
+get('/') do
+  "This application will calculate shipping costs"
+  erb(:input)
+end
+
+get('/output') do
+  erb(:output)
+end
---
+++ lib/parcels.rb
@@ -0,0 +1,17 @@
+class Parcel
+
+  def initialize(length, width, height, weight)
+    @length = length
+    @width = width
+    @heigth = height
+    @weight = weight
+  end
+
+  def volume(length, width, height, weight)
+
+  end
+
+  def cost_to_ship(length, width, height, weight)
+
+  end
+end
