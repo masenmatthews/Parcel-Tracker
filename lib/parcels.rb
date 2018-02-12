2e918eda81e35d3543099db9b4181c6c5c2ad042
Kaila Goff and Masen Matthews <masen@>
2018-02-12 11:43:08 -0800

    add volume method, build out backend

--- lib/parcels.rb
+++ lib/parcels.rb
@@ -7,11 +7,23 @@ class Parcel
     @weight = weight
   end
 
-  def volume(length, width, height, weight)
-
+  def volume(length, width, height)
+    @total_volume = (length * width * height)
+    if (total_volume <= 40)
+      total_volume_price == 10
+    elsif (total_volume > 40) && (total_volume <= 90)
+      total_volume_price == 15
+    elsif (total volume > 90)
+      total_volume_price == 20
+    else
+      "please enter a numerical value"
+  end
+    total_volume_price
   end
 
   def cost_to_ship(length, width, height, weight)
+    if ()
 
+    total_cost = volume_price + weight_price
   end
 end
