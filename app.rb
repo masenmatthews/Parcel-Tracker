8638f1b7ba63f1d4a3a318113ee1cbb9922d9848
Kaila Goff and Masen Matthews <masen@>
2018-02-12 14:35:05 -0800

    working volume function

--- app.rb
+++ app.rb
@@ -1,13 +1,18 @@
 require('sinatra')
 require('sinatra/reloader')
 require('pry')
-# require('./lib/parcels')
+require('./lib/parcels')
 
 get('/') do
   "This application will calculate shipping costs"
   erb(:input)
 end
 
-get('/output') do
+get('/result') do
+  @length = params.fetch("length").to_i
+  @width = params.fetch("width").to_i
+  @height = params.fetch("height").to_i
+  output = Parcel.new(@length, @width, @height)
+  @result = output.volume(@length, @width, @height)
   erb(:output)
 end
--- lib/parcels.rb
+++ lib/parcels.rb
@@ -1,29 +1,54 @@
 class Parcel
 
-  def initialize(length, width, height, weight)
+  def initialize(length, width, height)
     @length = length
     @width = width
     @heigth = height
-    @weight = weight
+    # @weight = weight
+    # @weight_price = weight_price
+    # @shipping_cost = shipping_cost
   end
 
   def volume(length, width, height)
-    @total_volume = (length * width * height)
-    if (total_volume <= 40)
-      total_volume_price == 10
-    elsif (total_volume > 40) && (total_volume <= 90)
-      total_volume_price == 15
-    elsif (total volume > 90)
-      total_volume_price == 20
-    else
-      "please enter a numerical value"
-  end
-    total_volume_price
-  end
-
-  def cost_to_ship(length, width, height, weight)
-    if ()
-
-    total_cost = volume_price + weight_price
+    if (@length * @width * @height = 1)
+      "its working now"
+    end
   end
 end
+
+#   def cost_to_ship()
+#     shipping_cost = @total_volume + @weight_price
+#   end
+#     return @shipping_cost
+# end
+
+
+# def volume(length, width, height)
+#   total_volume = (length * width * height)
+#   total_volume_price = 0
+#   if (total_volume <= 40)
+#     total_volume_price == 10
+#   elsif (total_volume > 40) && (total_volume <= 90)
+#     total_volume_price == 15
+#   elsif (total volume > 90)
+#     total_volume_price == 20
+#   else
+#     "please enter a numerical value"
+# end
+#   total_volume_price
+# end
+
+# def weight(weight)
+#   @weight_price = weight_price
+#   weight_price = 0
+#   if (weight <= 10)
+#     weight_price = 4
+#   elsif (weight >= 10) && (weight <= 25)
+#     weight_price = 7
+#   elsif (weight > 25)
+#     weight_price = 10
+#   else
+#     "Please enter valid numerical input"
+#   end
+#     weight_price
+# end
